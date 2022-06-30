8c804392bbe2142b7b4ad51189083aca83fef9ee
Danvero2022 <99843749+Danvero2022@users.noreply.github.com>
2022-06-30 17:59:29 +0100

    Pk

---
+++ 0x07-python-test_driven_development/tests/100-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+matrix_mul = __import__('100-matrix_mul').matrix_mul
+
+print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+[A[D[C[C[C[C[C[A[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[C[A[C[C[A[D[C[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[A[C[C[3~[3

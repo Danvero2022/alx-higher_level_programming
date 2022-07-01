c9d268f8e2e01bc66c80db45f4a44da672f540f5
Danvero2022 <99843749+Danvero2022@users.noreply.github.com>
2022-07-01 16:32:06 +0100

    kk

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,18 @@
-#!/usr/bin/python3
-"""
-Module add-integer
-Adds two integer together
-"""
+"""This module contains calculation functions"""
 
 
 def add_integer(a, b=98):
-    """Returns the addition of a and b,
-    or an error if a and b are not integers or floats
+    """"This function does the addition of 2 arguments
+    args:
+        a (union[int, float]): first number
+        b (union[int, float], optional): second number
+    returns:
+        the result of the addition
     """
-
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
+        raise TypeError('a must be an integer')
     if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
+        raise TypeError('b must be an integer')
 
     if type(a) is float:
         a = int(a)
